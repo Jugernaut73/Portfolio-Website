0c7a2c30207a567f93bd466c9eedc57bc0fa7054
Jugernaut73 <37919117+Jugernaut73@users.noreply.github.com>
2025-06-01 21:29:32 -0400

    Tech Card style changes

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -13,7 +13,7 @@ const TechCard = ({
   const { name, description, iconUrl, bgColor } = cardInfo;
 
   return (
-    <div className="flex flex-1 gap-5 p-2.5 rounded-xl bg-[#2e2a2a]/50 border border-[#302d2d] dark:bg-dark-200 hover:bg-[#2e2a2a] dark:hover:bg-dark-300 hover:border-[#E0E0E0] dark:hover:border-dark-700 transition-colors duration-200">
+    <div className="flex flex-1 gap-5 p-2.5 rounded-xl bg-[#2b2b2b]/50 border border-[#302d2d] dark:bg-dark-200 hover:bg-[#2b2b2b] dark:hover:bg-dark-300 hover:border-[#E0E0E0] dark:hover:border-dark-700 transition-colors duration-200">
       <div className={`p-3 rounded-lg w-fit`} style={{ backgroundColor: bgColor }}>
         <Image
           src={iconUrl}
